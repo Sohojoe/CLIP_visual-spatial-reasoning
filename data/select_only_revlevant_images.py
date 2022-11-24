b06cbe820f2a604f13d25cd7e7f9f5067ea80014
Joe Booth <joe@joebooth.com>
2022-11-24 10:33:57 -0800

    Initial commit

---
+++ data/select_only_revlevant_images.py
@@ -0,0 +1,26 @@
+import os
+import json
+import sys
+
+data_json = []
+with open(sys.argv[1], "r") as f:
+    lines = f.readlines()
+    for line in lines:
+        j_line = json.loads(line)
+        data_json.append(j_line)
+
+imgs = []
+for instance in data_json:
+    imgs.append(instance["image"])
+print (f"total data points: {len(imgs)}")
+imgs = list(set(imgs))
+print (f"total images: {len(imgs)}")
+
+for img in imgs:
+    if os.name == 'nt':
+        os.system(f"copy {os.path.join(sys.argv[2], img)} {sys.argv[3]}")
+    else:
+        os.system(f"cp {os.path.join(sys.argv[2], img)} {sys.argv[3]}")
+
+print (f"{len(os.listdir(sys.argv[3]))} images moved to {sys.argv[3]}")
+
